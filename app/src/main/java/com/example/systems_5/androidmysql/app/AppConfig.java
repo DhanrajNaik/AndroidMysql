3d7ef5135a7e06271777141238fa0bf289e2078b
DhanrajNaik <dhanrajnaik522@gmail.com>
2015-07-07 12:29:17 +0530

    login registration mysql database sqlite database using volley

---
+++ app/src/main/java/com/example/systems_5/androidmysql/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.systems_5.androidmysql.app;
+
+/**
+ * Created by System S-5 on 07-07-2015.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://www.djboy.16mb.com/android_login_api/index.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://www.djboy.16mb.com/android_login_api/index.php";
+}
